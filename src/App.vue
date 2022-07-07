3067a51468017b83e87033c35122edbf6ce963d7
晨曦 <c15293452071@163.com>
2022-07-07 12:09:30 +0800

    安装组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+  <XtxButton type="primary">msg</XtxButton>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入组件库
+import RabbitUi from 'rabbit-ui-core'
+import 'rabbit-ui-core/dist/rabbit-ui.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(RabbitUi).mount('#app')
