9245f9b5bdc13862fb0704acbbad91f94c5c1226
晨曦 <c15293452071@163.com>
2022-07-08 10:53:07 +0800

    项目公共样式设置引入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入重置样式的包
+import 'normalize.css'
+// 导入公用样式
+import '@/styles/common.less'
 // 导入组件库
 import RabbitUi from 'rabbit-ui-core'
 import 'rabbit-ui-core/dist/rabbit-ui.css'
